471c0e35436dd9f3ff1c73ee1d829d9b027f1650
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-04-21 16:52:46 +0600

    w3 School + zero to mstery + net ninja + jonas + maximilian

---
+++ w3 school course-JS/15.LOOP/2.For_loop/1.1.for.js
@@ -0,0 +1,5 @@
+const name = ['sami', 'saad', 'abu', 'abid'];
+
+for (let i = 0; i < name.length; i++){
+    console.log(name[i]);
+}
---
+++ w3 school course-JS/15.LOOP/3.If-else/1.1.if_else.js
@@ -0,0 +1,14 @@
+// let num = parseFloat(prompt("Enter a number : ","0")) ;
+
+if (num > 0) {
+   console.log("Positive");
+}
+if (num < 0) {
+   console.log("Negative");
+}
+if (num === 0) {
+   console.log("Zero");
+}
+
+// const name = prompt("What is your name?");
+// alert ("hellow ")
