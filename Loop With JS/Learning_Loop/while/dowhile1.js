cd8b05a2ec247177e8e39f2631d63727eedf770b
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-05-07 18:04:43 +0600

    while, dowhile loop done

---
+++ Loop With JS/Learning_Loop/while/dowhile1.js
@@ -0,0 +1,5 @@
+let i = 1;
+do{
+   console.log('Value of is is : ',i);
+   i++;
+}while(i < 5);
---
+++ Loop With JS/Learning_Loop/while/while2.js
@@ -0,0 +1,6 @@
+const names = ['sami','saad','abid','jesan'];
+let i = 0;
+while (i < names.length) {
+   console.log(names[i]);
+   i++;
+}
