4985e3a9e694debae9617a684cc5c6acf0a1c451
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-08-31 23:49:56 +0600

    Alhamdulillah

---
+++ w3 school course-JS/10.Function/string-uppercase(return).js
@@ -0,0 +1,5 @@
+function capital(str) {
+    return str.toUpperCase();
+}
+const result = capital('hello, i am sami');
+console.log(result);
---
+++ w3 school course-JS/10.Function/string-uppercase.js
@@ -0,0 +1,5 @@
+function uppercase(str) {
+    console.log(str.toUpperCase());
+}
+
+uppercase('hi, good morning Sam');
